330bcc18607eb416b24309bfb69d1ef098da15e5
it.soporte <it.soporte@zayro.com>
2022-08-10 20:54:44 -0500

    Conexcion con SQL Server
    Se agrego un Entity Framework de la base de datos DbSICatalogos
    Se importo la tabla Clientes

--- ModuloClientes/ModuloClientes/Controllers/DashboardController.cs
+++ ModuloClientes/ModuloClientes/Controllers/DashboardController.cs
@@ -21,45 +21,13 @@ namespace ModuloClientes.Controllers
 
         public ActionResult ClienteListado()
         {
+            DbIS_CatalogosEntities db = new DbIS_CatalogosEntities();
+
+            List<Clientes> listadoClientes = db.Clientes.ToList();
+
+            ViewBag.listadoClientes = listadoClientes;
+
             return View();
         }
     }
 }
-
-//< div class= "card card-danger" >
-
-//               < div class= "card-header" >
-
-//                  < h3 class= "card-title" > Different Width </ h3 >
-
-//                    </ div >
-
-//                    < div class= "card-body" >
-
-//                       < div class= "row" >
-
-//                          < div class= "col-3" >
-
-//                             < input type = "text" class= "form-control" placeholder = ".col-3" >
-
-//                               </ div >
-
-//                               < div class= "col-4" >
-
-//                                  < input type = "text" class= "form-control" placeholder = ".col-4" >
-
-//                                    </ div >
-
-//                                    < div class= "col-5" >
-
-//                                       < input type = "text" class= "form-control" placeholder = ".col-5" >
-
-//                                         </ div >
-
-//                                       </ div >
-
-//                                     </ div >
-
-//                                     < !-- /.card - body-- >
-
-//                                   </ div >
