2230bcd06da1acdeead261b1f6ebd090d402b48b
it.soporte <it.soporte@zayro.com>
2022-09-06 17:51:49 -0500

    Update modulo usuario
    Ya es posible modificar los datos de un usuario accediendo desde el listado para modificar

--- ModuloClientes/ModuloClientes/Controllers/UsuariosController.cs
+++ ModuloClientes/ModuloClientes/Controllers/UsuariosController.cs
@@ -8,12 +8,12 @@ namespace ModuloClientes.Controllers
 {
     public class UsuariosController : Controller
     {
-        public ActionResult UsuarioAlta(string id)
+        public ActionResult UsuarioAlta(int id)
         {
             DbIS_CatalogosEntities db = new DbIS_CatalogosEntities();
 
-            Clientes cliente = db.Clientes.Where(x => x.CLIENTE == id).SingleOrDefault();
-            ViewBag.cliente = cliente;
+            Usuarios usuario = db.Usuarios.Where(x => x.UsuID == id).SingleOrDefault();
+            ViewBag.usuario = usuario;
             ViewBag.id = id;
 
             return View();
@@ -67,6 +67,28 @@ namespace ModuloClientes.Controllers
             {
                 return Json(new { error = true, message = ex.Message }, JsonRequestBehavior.AllowGet);
             }
+        }
+
+
+        [HttpPost]
+        public JsonResult UsuarioModifica(Usuarios user)
+        {
+            try
+            {
+                DbIS_CatalogosEntities db = new DbIS_CatalogosEntities();
+                Usuarios nc = db.Usuarios.Where(x => x.UsuID == user.UsuID).SingleOrDefault();
+
+                db.Entry(nc).CurrentValues.SetValues(user);
+
+                db.SaveChanges();
+
+                return Json(new { error = false, message = "Correcto" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { error = true, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+
         }
     }
 }
